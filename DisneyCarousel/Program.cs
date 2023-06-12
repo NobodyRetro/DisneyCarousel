6159d8b37c339dee9e72a422c77be18843c3df60
NobodyRetro <59867447+NobodyRetro@users.noreply.github.com>
2023-06-12 16:21:48 -0700

    Add files via upload

---
+++ DisneyCarousel/Program.cs
@@ -0,0 +1,9 @@
+﻿/* You work on a particular carousel at Disney. You have to count from 3 to 0 on the screen before running the carousel.
+Write a program to output all the numbers from 3 to 0 */
+int x = 3;
+while (x >= 0)
+{
+    Console.WriteLine(x);
+    x --;
+}
+Console.ReadKey();
